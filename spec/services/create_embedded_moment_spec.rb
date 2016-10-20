0af17fa8c6ecf7ec65c6b58862e98ed14aa24f9d
fahrenhei7 <fahrenhei7lt@gmail.com>
2016-10-20 23:18:28 +0300

    added tests for service

--- spec/services/create_embedded_moment_spec.rb
+++ spec/services/create_embedded_moment_spec.rb
@@ -1,10 +1,26 @@
 require 'rails_helper'
 
 describe CreateEmbeddedMoment do
-  describe 'api calls' do
-    it 'twitter API'
+  describe 'oembed calls' do
+    it 'twitter' do
+      twitter_oembed = instance_double(TwitterOEmbed)
+      expect(TwitterOEmbed).to receive(:new).with({ src: 'mysrc' })
+      expect(twitter_oembed).to receive(:fetch).with('mysrc')
+      subject.new({ src: 'mysrc' }).call
+    end
   end
-  describe 'changes database on successful request'
-  describe 'does not change database on unseccessful request'
-
+  it 'changes database on successful request' do
+    twitter_oembed = instance_double(TwitterOEmbed)
+    allow(twitter_oembed).to receive(:call) { Hash.new }
+    expect {
+      twitter_oembed.call
+    }.to change(EmbeddedMoment, :count).by(1)
+  end
+  describe 'does not change database on unseccessful request' do
+    twitter_oembed = instance_double(TwitterOEmbed)
+    allow(twitter_oembed).to receive(:call) { nil }
+    expect {
+      twitter_oembed.call
+    }.not_to change(EmbeddedMoment, :count)
+  end
 end
