e7015713b01eff6f8388a436bb94722fc92f1b60
fahrenhei7 <fahrenhei7lt@gmail.com>
2016-11-02 02:52:52 +0300

    annotated!

--- app/models/native_moment.rb
+++ app/models/native_moment.rb
@@ -2,11 +2,15 @@
 #
 # Table name: native_moments
 #
-#  id         :integer          not null, primary key
-#  body       :text
-#  story_id   :integer
-#  created_at :datetime         not null
-#  updated_at :datetime         not null
+#  id                   :integer          not null, primary key
+#  body                 :text
+#  story_id             :integer
+#  created_at           :datetime         not null
+#  updated_at           :datetime         not null
+#  picture_file_name    :string
+#  picture_content_type :string
+#  picture_file_size    :integer
+#  picture_updated_at   :datetime
 #
 
 class NativeMoment < ApplicationRecord
