280152235586d11c0414c38ad7fb74e833cc0880
fahrenhei7 <fahrenhei7lt@gmail.com>
2016-11-09 15:51:37 +0300

    annotated

--- app/models/story.rb
+++ app/models/story.rb
@@ -2,12 +2,16 @@
 #
 # Table name: stories
 #
-#  id          :integer          not null, primary key
-#  title       :string
-#  description :text
-#  user_id     :integer
-#  created_at  :datetime         not null
-#  updated_at  :datetime         not null
+#  id                   :integer          not null, primary key
+#  title                :string
+#  description          :text
+#  user_id              :integer
+#  created_at           :datetime         not null
+#  updated_at           :datetime         not null
+#  picture_file_name    :string
+#  picture_content_type :string
+#  picture_file_size    :integer
+#  picture_updated_at   :datetime
 #
 
 class Story < ApplicationRecord
