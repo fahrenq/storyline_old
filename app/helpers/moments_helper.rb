6397f26edb718ae3658d852a97e3906389c24e8d
fahrenhei7 <fahrenhei7lt@gmail.com>
2016-11-13 13:06:50 +0300

    small refactor

--- app/helpers/moments_helper.rb
+++ app/helpers/moments_helper.rb
@@ -1,9 +1,6 @@
 module MomentsHelper
   def render_moment(moment)
-    if moment.is_a?(NativeMoment)
-      render partial: 'web/shared/moments/native_moment', locals: { moment: moment }
-    elsif moment.is_a?(EmbeddedMoment)
-      render partial: 'web/shared/moments/embedded_moment', locals: { moment: moment }
-    end
+    render partial: "web/shared/moments/#{moment.class.to_s.underscore}",
+           locals: { moment: moment }
   end
 end
