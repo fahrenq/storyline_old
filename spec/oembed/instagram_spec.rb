517d305544c4339cda7e8190e64f6893cf6f4eb7
fahrenhei7 <fahrenhei7lt@gmail.com>
2016-11-14 20:36:20 +0300

    added instagram oembed support + tests :sparkles:

---
+++ spec/oembed/instagram_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+require 'oembedapi/instagram'
+
+describe OembedApi::Instagram, :vcr do
+  it 'receives successful response' do
+    response = subject.fetch('https://www.instagram.com/p/BMCEzS6hd4P/?taken-by=heroku')
+    expect(response.is_a?(Hash)).to be_truthy
+  end
+  it 'reveives nil on invalid tweet adress' do
+    response = subject.fetch('https://www.instagramcm.sr/pBMCEzS6hd4/?ake-y=heroku')
+    expect(response.nil?).to be_truthy
+  end
+end
