921c94d4184f3802979c23bb92f68c89b235cfcc
fahrenhei7 <fahrenhei7lt@gmail.com>
2016-11-15 21:29:43 +0300

    added smtp configuration to development :wrench:

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -45,6 +45,16 @@ Rails.application.configure do
   # Suppress logger output for asset requests.
   config.assets.quiet = true
 
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    domain:               'localhost:3000',
+    user_name:            ENV['GMAIL_USERNAME'],
+    password:             ENV['GMAIL_PASSWORD'],
+    # authentication:       'plain',
+    enable_starttls_auto: true  }
+
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 
