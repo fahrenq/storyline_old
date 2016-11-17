e316cbec25d70f6b57637c5f2a84ff781b908f2e
fahrenhei7 <fahrenhei7lt@gmail.com>
2016-11-17 23:08:45 +0300

    fixed for CI, deleted byebug

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -48,7 +48,6 @@ Rails.application.configure do
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.default_url_options = { host: 'localhost:3000' }
-  byebug
   config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
     port:                 587,
