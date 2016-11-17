f75e3e745b4316180de28f1b3dc533f971feeaf1
fahrenhei7 <fahrenhei7lt@gmail.com>
2016-11-17 22:07:39 +0300

    changed env variables

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -52,9 +52,9 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
     port:                 587,
-    # domain:               ENV['GMAIL_DOMAIN'],
-    user_name:            ENV['GMAIL_USERNAME'],
-    password:             ENV['GMAIL_PASSWORD'],
+    # domain:               ENV['MAIL_DOMAIN'],
+    user_name:            ENV['MAIL_USERNAME'],
+    password:             ENV['MAIL_PASSWORD'],
     authentication:       :plain,
     enable_starttls_auto: true
   }
--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -12,7 +12,7 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = '"User management" <storyline.q@gmail.com>'
+  config.mailer_sender = "\"User management\" <#{ENV['MAIL_USERNAME']}>"
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
