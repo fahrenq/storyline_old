84a0711f41eba15cbd8e2338f331622b000c5441
fahrenhei7 <fahrenhei7lt@gmail.com>
2016-11-23 00:24:03 +0300

    Deleted unnecessary lines in spec support file

--- spec/support/user/registration_form.rb
+++ spec/support/user/registration_form.rb
@@ -1,9 +1,6 @@
 class RegistrationForm
   include Capybara::DSL
 
-  AVATAR_FILENAME = 'user_avatar.png'
-  AVATAR_PATH = "#{Rails.root}/spec/fixtures/#{AVATAR_FILENAME}"
-
   def visit_page_direct
     visit('/registration')
     self
@@ -20,7 +17,6 @@ class RegistrationForm
     password = Faker::Internet.password(8)
     fill_in('Name', with: params.fetch(:name, Faker::Internet.user_name))
     fill_in('Email', with: params.fetch(:email, Faker::Internet.email))
-    attach_file('Avatar', params.fetch(:avatar, AVATAR_PATH))
     fill_in('Password', with: params.fetch(:password, password), match: :prefer_exact)
     fill_in('Password confirmation', with: params.fetch(:password_confirmation, password), match: :prefer_exact)
     self
