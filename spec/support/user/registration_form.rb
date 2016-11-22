a4cde519e4ca477592cad2f5b039b210d4cd3ed0
fahrenhei7 <fahrenhei7lt@gmail.com>
2016-11-22 11:17:30 +0300

    Add test for sign_up avatar set :white_check_mark:

--- spec/support/user/registration_form.rb
+++ spec/support/user/registration_form.rb
@@ -1,6 +1,9 @@
 class RegistrationForm
   include Capybara::DSL
 
+  AVATAR_FILENAME = 'user_avatar.png'
+  AVATAR_PATH = "#{Rails.root}/spec/fixtures/#{AVATAR_FILENAME}"
+
   def visit_page_direct
     visit('/registration')
     self
@@ -14,9 +17,10 @@ class RegistrationForm
   # end
 
   def fill_in_with(params = {})
+    password = Faker::Internet.password(8)
     fill_in('Name', with: params.fetch(:name, Faker::Internet.user_name))
     fill_in('Email', with: params.fetch(:email, Faker::Internet.email))
-    password = Faker::Internet.password(8)
+    attach_file('Avatar', params.fetch(:avatar, AVATAR_PATH))
     fill_in('Password', with: params.fetch(:password, password), match: :prefer_exact)
     fill_in('Password confirmation', with: params.fetch(:password_confirmation, password), match: :prefer_exact)
     self
