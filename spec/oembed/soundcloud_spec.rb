2a8f6d2b4d40ac4b186645ed3eaf183d799649b3
fahrenhei7 <fahrenhei7lt@gmail.com>
2016-11-24 22:58:50 +0300

    Added soundcloud oembed support

---
+++ spec/oembed/soundcloud_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+require 'oembedapi/soundcloud'
+
+describe OembedApi::Soundcloud, :vcr do
+  it 'receives successful response' do
+    response = subject.fetch('https://soundcloud.com/21savage/x-feat-future')
+    expect(response.is_a?(Hash)).to be_truthy
+  end
+  it 'receives nil on invalid adress' do
+    response = subject.fetch('https://oundc.loudom1savageeat-future')
+    expect(response.nil?).to be_truthy
+  end
+end
