928e917c6f2bf619ec4db80c3c8646513c87efd1
fahrenhei7 <fahrenhei7lt@gmail.com>
2017-01-16 00:59:15 +0300

    Added flash elements

--- app/assets/javascripts/global.js
+++ app/assets/javascripts/global.js
@@ -4,4 +4,31 @@ $(document).on('turbolinks:load', () => {
   $('#sidebar-opener').on('click', () => {
     $('.ui.sidebar').sidebar('toggle');
   });
+
+  showFlash();
+
+  function showFlash() {
+    if ($('header .flash-element').length > 0) {
+      sleep(600).then(logoToFlash);
+      sleep(8000).then(flashToLogo);
+    };
+  };
+
+  function logoToFlash() {
+    $('header .logo').transition('vertical flip', 500, () => {
+      $('header .flash-element').transition('vertical flip');
+    });
+  };
+
+  function flashToLogo() {
+    $('header .flash-element').transition('vertical flip', 500, () => {
+      $('header .logo').transition('vertical flip');
+    });
+  };
+
+  function sleep(time) {
+    return new Promise((resolve) => setTimeout(resolve, time));
+  };
+
 });
+
